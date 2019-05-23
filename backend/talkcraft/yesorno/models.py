dcc4a0cc46373afcdd1ad2f43885625929b0d23c
good-engineer <ms_danesh@hotmail.com>
2019-05-23 15:13:46 +0900

    updated rank page settings

--- backend/talkcraft/yesorno/models.py
+++ backend/talkcraft/yesorno/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+
 class YesOrNo(models.Model):
     objects = models.Manager()
     created = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey('auth.User', related_name='yesorno', on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        'auth.User', related_name='yesorno', on_delete=models.CASCADE)
     title = models.CharField(max_length=50, blank=True, default='')
     mainTopic = models.CharField(max_length=100, blank=True, default='')
     live = models.BooleanField(default=False)
@@ -11,14 +13,14 @@ class YesOrNo(models.Model):
     summary = models.TextField()
     textDescriptionA = models.TextField()
     textDescriptionB = models.TextField()
-    #Need to set max and min Value
-    wordLimit = models.IntegerField(default = 150)
-    #Need to set max and min value
-    totalTimeLimit = models.DurationField(default = 15) 
-    #categorySelection = 
+    # Need to set max and min Value
+    wordLimit = models.IntegerField(default=150)
+    # Need to set max and min value
+    totalTimeLimit = models.DurationField(default=15)
+    # categorySelection =
     pictureA = models.ImageField(blank=True, upload_to="yesorno/%Y/%m/%d")
     pictureB = models.ImageField(blank=True, upload_to="yesorno/%Y/%m/%d")
-    #agreementBox =  
+    # agreementBox =
 
     class Meta:
         ordering = ('created', )
@@ -26,9 +28,15 @@ class YesOrNo(models.Model):
     def __str__(self):
         return str(self.author) + "_" + str(self.mainTopic)
 
-#Need to do few more saving
+    def count_YesOrNo_of(self):
+        return YesOrNo.objects.filter(author=self.author).count()
+
+
+# Need to do few more saving
+
+
 class UserComment(models.Model):
-    user = models.OneToOneField(to='user', on_delete=models.CASCADE)   
-    createdTime = models.DateTimeField(auto_now_add = True)
+    user = models.OneToOneField(to='user', on_delete=models.CASCADE)
+    createdTime = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
-    id = models.AutoField(primary_key = True)
+    id = models.AutoField(primary_key=True)
